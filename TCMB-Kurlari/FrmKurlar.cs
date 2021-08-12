dbac91c229f984839fd7f4a85e7fc1c844d58fab
Umut D <umutdokel@gmail.com>
2021-08-12 04:17:20 +0300

    1.16

---
+++ TCMB-Kurlari/FrmKurlar.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using TcmbLibrary;
+
+namespace TcmbUI
+{
+    public partial class FrmTcmbKurlari : Form
+    {
+        private readonly Tarih _tarih;
+        private readonly Internet _internet;
+
+        public FrmTcmbKurlari()
+        {
+            InitializeComponent();
+
+            _tarih = new Tarih();
+            _internet = new Internet(_tarih);
+        }
+
+        private void FrmTcmbKurlari_Load(object sender, EventArgs e)
+        {
+            DateTimePickerTarihAraligi();
+
+            if (_internet.BaglantiVarMi())
+                dtpTarih.Click += DtpTarih_ValueChanged;
+        }
+
+        private void DateTimePickerTarihAraligi()
+        {
+            dtpTarih.MaxDate = DateTime.Today;
+            dtpTarih.MinDate = new DateTime(1996, 05, 02);
+        }
+
+        private void DtpTarih_ValueChanged(object sender, EventArgs e)
+        {
+            _tarih.SecilenGun(dtpTarih.Value);
+            string webLink = _internet.OlusturulanWebAdresi();
+
+            try
+            {
+                DataGridOlustur(webLink);
+                tsDurum.Text = new XmlBelge().BultenBilgisiAl(webLink);
+            }
+            catch (Exception)
+            {
+                dataGridGorunumu.DataSource = null;
+
+                MessageBox.Show(@"İstem yaptığınız güne dair herhangi bir veri bulunamadı. Bunun iki olası sebebi var:;"
+                                + Environment.NewLine +
+                                @"1) Bulunulan günün saati 15:30'dan önce olabilir. Zira TCMB, bulunulan günün verilerini 15:30 itibariyle paylaşıma sunmakta."
+                                + Environment.NewLine +
+                                @"2) Seçilen gün; hafta sonuna, resmi tatil veya bayram tatiline denk gelmiş olabilir.",
+                    @"Gün Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DataGridOlustur(string link)
+        {
+            DataSetIslem dataSetIslem = new DataSetIslem();
+            dataGridGorunumu.DataSource = dataSetIslem.Olustur(link).Tables[1];
+
+            DataGridIslem dataGrid = new DataGridIslem();
+            dataGrid.Gorunum(dataGridGorunumu);
+        }
+
+        private void MenuSayfayaGit_Click(object sender, EventArgs e)
+        {
+            Process.Start(_internet.OlusturulanWebAdresi());
+        }
+
+        private void MenuHakkinda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"Bu program ile Türkiye Cumhuriyeti Merkez Bankası'nın (TCMB) resmi web sitesinde yayınlanan ve '2 Mayıs 1996’dan bugüne değin' sunulan döviz alış/satış ve döviz efektif alış/satış bilgilerini görebilirsiniz.",
+                @"Hakkında", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void MenuCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ TCMB-Kurlari/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Derleme hakkında Genel Bilgiler aşağıdakilerle denetlenir. 
+// kontrol edilir. Derlemeyle ilişkilendirilen bilgiyi değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("TCMB Kurları")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TCMB Kurları")]
+[assembly: AssemblyCopyright("Umut D. | 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible yanlış olarak ayarlamak bu derlemedeki türleri görünmez hale getirir 
+// COM bileşenleri için.  Bu derlemedeki bir türe erişmeniz gerekiyorsa 
+// şu türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Eğer bu proje COM'a maruz kaldıysa aşağıdaki GUID typelib'in IDsi içindir
+[assembly: Guid("329940e8-9ea8-4716-9857-102cf52ad61d")]
+
+// Bir derlemenin sürüm bilgisi bahsedilen dört değerden meydana gelir:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Sayısı
+//      Düzeltme
+//
+// Bütün değerleri belirtebilir ya da Yapı ve Düzeltme Numaralarını varsayılan olarak seçebilirsiniz 
+// Yapı Numaralarını varsayılan yapabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.6.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
